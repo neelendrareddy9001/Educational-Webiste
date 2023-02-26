db64cd7352294463639b5a35955f3722b40184bf
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-02-26 14:47:46 +0100

    Educational-Website

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.css';
+import {Container} from 'reactstrap';
+
+const Header = () => {
+    return (
+        <section>
+            <Container>
+                
+            </Container>
+        </section>
+    )
+};
+
+export default Header;
