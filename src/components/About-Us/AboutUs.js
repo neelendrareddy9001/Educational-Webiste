fc5992dc5ba57c907c407a29a8dcaa03f84c9b4f
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-27 21:54:09 +0100

    Update AboutUs.js

--- src/components/About-Us/AboutUs.js
+++ src/components/About-Us/AboutUs.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './about.css';
+import { Container, Row, Col } from 'reactstrap';
+import aboutImg from '../../assets/images/about-us.png';
+
 
 const AboutUs = () => {
-  return (
-    <div>AboutUs</div>
-  )
+    return (
+        <section>
+            <Container>
+                <Row>
+                    <Col lg="6" md="6">
+                        <img src={aboutImg} alt="" className="w-100"></img>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
 }
 
 export default AboutUs
