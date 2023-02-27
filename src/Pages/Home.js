6cc7e5a9a1373d277fbaa59c1295de7020d98eb8
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-27 21:55:10 +0100

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Header from '../components/Header/Header';
+import HeroSection from '../components/Hero-Section/Hero-section';
+import Company from '../components/Company-Section/Company';
+import AboutUs from '../components/About-Us/AboutUs';
 
 const Home = () => {
-    return <h1>Home Page</h1>
+    return (
+        <Fragment>
+            <Header/>
+            <HeroSection/>
+            <Company/>
+            <AboutUs/>
+        </Fragment>
+    )
 }
 
 export default Home;
