94d36f40ca2aa7304da52d8e33dc7bf1a1f62ec3
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-28 10:38:07 +0100

    Add files via upload

---
+++ src/components/Course-Section/CourseCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const CourseCard = (props) => {
+
+    const {imgUrl, title, lesson, students, rating} = props.item;
+
+    return (
+        <div className='single__item'>
+                        <div className='course__img'>
+                            <img src={imgUrl} alt="" className='w-100'/>
+                        </div>
+                        <div className='course__details'>
+                            <h5 className='course__title mb-4' >
+                                {title}
+                            </h5>
+
+                            <div className='d-flex justify-content-between align-items-center'>
+                                <p className='lesson d-flex align-items-center gap-1'>
+                                    <i class="ri-book-open-line"></i>{lesson} Lessons
+                                </p>
+                                <p className='students d-flex align-items-center gap-1'>
+                                    <i class="ri-user-line"></i>{students}k
+                                </p>
+                            </div>
+                            <div className='d-flex justify-content-between align-items-center'>
+                                <p className='lesson d-flex align-items-center gap-1'>
+                                    <i class="ri-star-fill"></i>{rating}k
+                                </p>
+                                <p className='enroll d-flex align-items-center gap-1'>
+                                    <a href='#'>Enroll Now</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+    )
+}
+
+export default CourseCard
