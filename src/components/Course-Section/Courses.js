87957e76ab2cf5833980f088668fc774a69e9ef7
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-28 10:40:24 +0100

    Update Courses.js

--- src/components/Course-Section/Courses.js
+++ src/components/Course-Section/Courses.js
@@ -7,6 +7,35 @@ import './Courses.css';
 import CourseCard from './CourseCard';
 
 
+const coursesData = [
+    {
+      id: "01",
+      title: "Web Design BootCamp-2022 for Beginners",
+      lesson: 12,
+      students: 12.5,
+      rating: 5.9,
+      imgUrl: courseImg1,
+    },
+  
+    {
+      id: "02",
+      title: "Professional Graphics Design, PhotoShop, Adobe XD, Figma",
+      lesson: 12,
+      students: 12.5,
+      rating: 5.9,
+      imgUrl: courseImg2,
+    },
+  
+    {
+      id: "03",
+      title: "UI/UX BootCamp for Beginners in 2022",
+      lesson: 12,
+      students: 12.5,
+      rating: 5.9,
+      imgUrl: courseImg3,
+    },
+  ];
+
 const Courses = () => {
     return (
         <Container>
