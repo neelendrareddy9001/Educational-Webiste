ff8a842f04f053a660f873f734bc050e8906dff4
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-28 10:39:52 +0100

    Update Courses.js

--- src/components/Course-Section/Courses.js
+++ src/components/Course-Section/Courses.js
@@ -4,6 +4,7 @@ import courseImg1 from '../../assets/images/web-design.png';
 import courseImg2 from '../../assets/images/graphics-design.png';
 import courseImg3 from '../../assets/images/ui-ux.png';
 import './Courses.css';
+import CourseCard from './CourseCard';
 
 
 const Courses = () => {
@@ -11,31 +12,17 @@ const Courses = () => {
         <Container>
             <Row>
                 <Col lg="4">
-                    <div className='single__item'>
-                        <div className='course__img'>
-                            <img src={courseImg1} alt="" className='w-100'/>
+                    <div className='course__top d-flex justify-content-between align-items-center'>
+                        <div className='course__top__left w-50'>
+                            <h2>Our Popular Courses</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+                                consequatur libero quod voluptatibus ullam quia quas, vitae
+                                voluptatem recusandae reprehenderit!
+                            </p>
                         </div>
-                        <div className='course__details'>
-                            <h5 className='course__title'>
-                                Web Design Bootcamp-2022 for Beginners
-                            </h5>
-
-                            <div className='d-flex justify-content-between align-items-center'>
-                                <p className='lesson d-flex align-items-center gap-1'>
-                                    <i class="ri-book-open-line"></i>12 Lessons
-                                </p>
-                                <p className='students d-flex align-items-center gap-1'>
-                                    <i class="ri-user-line"></i>12.5k
-                                </p>
-                            </div>
-                            <div className='d-flex justify-content-between align-items-center'>
-                                <p className='lesson d-flex align-items-center gap-1'>
-                                    <i class="ri-star-fill"></i>5.9k
-                                </p>
-                                <p className='enroll d-flex align-items-center gap-1'>
-                                    <a href='#'>Enroll Now</a>
-                                </p>
-                            </div>
+                        <div className='w-50 text-end'>
+                            <button className='btn'>See All</button>
                         </div>
                     </div>
                 </Col>
